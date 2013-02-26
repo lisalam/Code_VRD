675ef37e7e053637129b019a7d1553fbc0897c1a
Lisa Lamasse <lisalamasse@gmail.com>
2013-02-26 17:52:26 +0100

    Création de dictionnaires et de listes, attention, étape pas testée !!!! syntaxe ok

--- Data.py
+++ Data.py
@@ -21,6 +21,7 @@ class Data():
 	
 	def __init__(self, projet):	
 		self.__projet = projet
+		self.__listboites=self.__getlisteboites()
 		self.__dicoText = dict()
 		self.__boiteGene = dict()
 		self.__boiteN = dict()
@@ -31,30 +32,97 @@ class Data():
 
 	def __run(self) :
 
-		if self.__builddicoText() == "erreur dossiers" : return "erreur dossiers"
-		else : self.__dicoText = self.__builddicoText()
+		if self.__getdicoText() == "erreur dossiers" : return "erreur dossiers"
+		else : self.__dicoText = self.__getdicoText()
+
+		if self.__getboiteGene() == "erreur dossiers" : return "erreur dossiers"
+		else : self.__boiteGene == self.__getboiteGene()
+
+		if self.__getboiteN() == "erreur dossiers" : return "erreur dossiers"
+		else : self.__boiteN == self.__getboiteN()
+
+		if self.__getboiteB() == "erreur dossiers" : return "erreur dossiers"
+		else : self.__boiteB == self.__getboiteB()
+
+		if self.__getboitecond() == "erreur dossiers" : return "erreur dossiers"
+		else : self.__boitecond == self.__getboitecond()
 		
-		self.__boiteGene = self.__buildboiteGene()
 
 		return "ok"
 
-	def __builddicoText(self) :
+
+	def __getboitecond(self):
+		for i in range(len(self.__listboites)):
+			pass
+			#self.__getboitecond[self.__listboites[i]]=listecond
+
+		
+	
+	def __getboiteN(self):
+		tempdico=dict()
+		for i in range(len(self.__listboites)):
+			boite=self.__listboites[i]
+			f=self.__dicoText[boite]
+			fichier=open(f,"r")
+			alllines = fichier.readlines()[0]
+			ligne = alllines.split("\r")[1]
+			ligne = ligne.split("\t")
+			nb = ligne[4]
+			tempdico[boite]=(nb, boite)
+		return tempdico
+
+		
+
+		
+	def __getboiteB(self):
+		tempdico=dict()
+		for i in range(len(self.__listboites)):
+			boite=self.__listboites[i]
+			f=self.__dicoText[boite]
+			fichier=open(f,"r")
+			alllines = fichier.readlines()[0]
+			ligne = alllines.split("\r")[1]
+			ligne = ligne.split("\t")
+			nb = ligne[4]
+			tempdico[nb]=(nb, boite)
+		return tempdico
+
+
+	def __getdicoText(self) :
 		tempdico=dict()
 		tempboites=os.listdir(self.__projet) # récupération listes de dossiers et des fichiers
 		if len(tempboites)<2 : return "erreur dossiers"
 		listpaths=[os.path.join(self.__projet,v) for v in tempboites if os.path.isdir(os.path.join(self.__projet,v))] # liste en intensions, ne garde que les dossiers et les transforment en "path" complet 
-		listboites=[v for v in tempboites if os.path.isdir(os.path.join (self.__projet,v))] # tous les dossiers de mon répertoire
-		for i in range(len(listboites)):
-			mot=os.path.join(listpaths[i],"Texte",(listboites[i]+"_labels.txt"))
+		for i in range(len(self.__listboites)):
+			mot=os.path.join(listpaths[i],"Texte",(self.__listboites[i]+"_labels.txt"))
 			if os.path.isfile(mot):
-				tempdico[listboites[i]]=mot
+				tempdico[self.__listboites[i]]=mot
 
 		return tempdico
 
 		
 
-	def __buildboiteGene(self) :
+	def __getboiteGene(self) :
 		pass
+
+	def __getDicoLignes(self, boite) :
+		f = self.__dicoText[boite]
+		fichier = open(f,"r") #ouvrir le fichier en lecture seule
+		alllines=fichier.readlines() # lire toutes les lignes du fichier
+		alllines=alllines[0].split("\r") # on veut lire la première ligne [0] et séparer les éléments de la ligne (ici le fichier excel fait que l'on a qu'une ligne dans tout le fichier)
+		dicoligne=dict() # création d'un dictionnaire pour les lignes
+		for i in range(1,len(alllines)): # boucle avec index
+			ligne=alllines[i].split("\t")
+			dicoligne[i]=ligne # ligne est une liste pour laquelle chaque valeur est une des colonnes du fichier texte initial
+
+		return dicoligne
+
+	def __getlisteboites(self):
+		tempboites=os.listdir(self.__projet) # récupération listes de dossiers et des fichiers
+		listboites=[v for v in tempboites if os.path.isdir(os.path.join (self.__projet,v))] # tous les dossiers de mon répertoire
+		return listboites
+	
+
 	
 #**************** Fin de la classe Exemple ******
 
