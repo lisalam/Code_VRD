ef4ab270089b60313cbe2ae462c3b2c275c7467d
Lisa Lamasse <lisalamasse@gmail.com>
2013-03-11 17:10:36 +0100

    Début de la classe puit

---
+++ Puit.py
@@ -0,0 +1,16 @@
+# -*- coding: iso-8859-15 -*-
+
+import sys
+import os
+import os.path as path
+import getpass
+
+
+username=getpass.getuser()
+
+mypath=os.path.expanduser(os.path.join("~","Dropbox","Macros_Lisa","Code_VRD"))
+sys.path.append(mypath)
+
+from org.python.core import codecs
+codecs.setDefaultEncoding('utf-8')
+
