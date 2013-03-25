d1ce018735e855b17c26d7341d9b2428d1290c6b
Lisa Lamasse <lisalamasse@gmail.com>
2013-03-25 17:49:14 +0100

    Avancement de la classe Boite, j'ai rempli les listes des gènes et des conditions et essayer de faire le PUITS_96 et le dicoWell ...

--- Boite.py
+++ Boite.py
@@ -15,6 +15,8 @@ from org.python.core import codecs
 codecs.setDefaultEncoding('utf-8')
 
 from Data import Data
+from Well import Well
+
 
 # ****************************** Début de la classe "Boite" ******************************
 
@@ -28,6 +30,7 @@ class Boite(object):
 		self.__dicoWell = dict()
 		self.__projet = projet
 		self.__data = Data(self.__projet)
+		self.__PUITS_96()
 
 		print self.__run()
 
@@ -42,22 +45,31 @@ class Boite(object):
 
 		if self.__getdicoWell() == "erreur puit" : return "erreur puit"
 		else : self.__dicoWell=self.__getdicoWell() 
-	
+
 		
 		return "ok"
 
 
 	
 	def __getlistGene(self):
-		pass
-		
+		temp = self.__data.dicoG
+		return temp[self.__nomboite]
 	
 	def __getlistCond(self):
-		pass
+		temp = self.__data.dicoC
+		return temp[self.__nomboite]
 		
 			
 	def __getdicoWell(self):
 		dicolignes = self.__data.getdicolignes(self.__nomboite)
+		ensDedans = set()
+		ensDehors = set()
+		if val in self.__PUITS_96 :
+			ensDedans.add(val)
+		else : ensDehors.add(val)
+		ensDedans.difference(ensDehors)
+		
+		
 		#print dicolignes.keys()
 		#print dicolignes[0], dicolignes[1]
 		
@@ -69,6 +81,22 @@ class Boite(object):
 	def __getProjet(self) : return self.__projet
 
 	def __setProjet(self, projet) : self.__projet = projet
+
+	@staticmethod
+	def PUITS_96(self) :
+		templist = list()
+		i = "1, 2, 3, 4, 5, 6, 7, 8"
+		j = "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12"
+		p = Well(i,j)
+		for i in self.__PUITS_96:
+			print i
+		for j in self.__PUITS_96:
+			print j
+		templist.append(p)
+		templist.sort()
+
+		return templist
+		
 
 
 # ------------------------------ Propriétés de la classe "Boite" ------------------------------
@@ -91,6 +119,7 @@ if __name__ == "__main__" :
 	#print boite.dicoW
 	#print boite.nom
 	#print boite.projet
+	
 	
 
 
