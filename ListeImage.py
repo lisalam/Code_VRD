4b89e8bab67018aa32de637ee3c4d0efa3645c33
Lisa Lamasse <lisalamasse@gmail.com>
2013-04-08 16:32:52 +0200

    Fenetre terminée, faire le lien avec le futur contrôleur self ok

--- ListeImage.py
+++ ListeImage.py
@@ -30,9 +30,10 @@ codecs.setDefaultEncoding('utf-8')
 
 class ListeImage(swing.JFrame, swing.Scrollable):
 
-		def __init__(self):
+		def __init__(self, listimp):
 			swing.JFrame.__init__(self, title="Images")
 			self.setDefaultCloseOperation(swing.JFrame.DISPOSE_ON_CLOSE)
+			self.__listimp = listimp
 			self.run()
 
 		def run(self):
@@ -42,25 +43,25 @@ class ListeImage(swing.JFrame, swing.Scrollable):
 
 
 			Panel1=swing.JPanel(awt.FlowLayout(awt.FlowLayout.CENTER))
-			#Panel1.setBorder(line)
+			Panel1.setBorder(line)
 			label=swing.JLabel("")
 			label.setText("Liste des Images")
 			Panel1.add(label)
-
+			
 
 			Panel2=swing.JPanel(awt.FlowLayout(awt.FlowLayout.CENTER))
-			#Panel2.setBorder(line)
-			#self.__dispImage = swing.JTextField(preferredSize=(100, 50), horizontalAlignment=swing.SwingConstants.LEFT)
-			#self.__dispImage.text = ""
-			#Panel2.add(self.__dispImage)
-
-			listimages = swing.JList(["Started ListeImage.py at Wed Mar 27 16:58:30 CET 2013", "Started ListeImage.py at Wed Mar 27 16:59:15 CET 2013", "Started ListeImage.py at Wed Mar 27 17:11:33 CET 2013"])
-			Panel2.add(listimages)
+			Panel2.setBorder(line)
+			listtitles=[imp.getTitle() for imp in self.__listimp]
+			self.__listimages = swing.JList(listtitles)
+			barre = swing.JScrollPane(self.__listimages)
+			Panel2.add(barre)
 			
 			Panel3=swing.JPanel(awt.FlowLayout(awt.FlowLayout.RIGHT))
-			#Panel3.setBorder(line)
-			select = swing.JButton("Select", actionPerformed=self.__select)
+			Panel3.setBorder(line)
+			select = swing.JButton("Show Selected", actionPerformed=self.__select)
 			Panel3.add(select)
+			hide = swing.JButton("Hide Selected", size=(100, 70), actionPerformed=self.__hide)
+			Panel3.add(hide)
 			close = swing.JButton("Close", size=(100, 70), actionPerformed=self.__close)
 			Panel3.add(close)
 			
@@ -68,10 +69,16 @@ class ListeImage(swing.JFrame, swing.Scrollable):
 			self.contentPane.add(Panel2, awt.BorderLayout.CENTER)
 			self.contentPane.add(Panel3, awt.BorderLayout.SOUTH)
 			
-			
+		def __hide(self, event):
+			titles = self.__listimages.getSelectedValues()
+			for imp in self.__listimp :
+				if imp.getTitle() in titles : imp.hide()
 			
 		def __select(self, event):
-			pass	
+			titles = self.__listimages.getSelectedValues()
+			for imp in self.__listimp :
+				if imp.getTitle() in titles : imp.show()
+				
 	
 
 		def __close(self, event):
@@ -81,5 +88,26 @@ class ListeImage(swing.JFrame, swing.Scrollable):
 
 			
 if __name__ == "__main__":
-	images = ListeImage()
-	images.show()
+	listimp=[]
+	imp1 = ImagePlus("/Users/lisalamasse/Dropbox/Macros_Lisa/ManipsTM/cells/cell0000.tif")
+	imp2 = ImagePlus("/Users/lisalamasse/Dropbox/Macros_Lisa/ManipsTM/cells/cell0001.tif")
+	imp3 = ImagePlus("/Users/lisalamasse/Dropbox/Macros_Lisa/ManipsTM/cells/cell0002.tif")
+	imp4 = ImagePlus("/Users/lisalamasse/Dropbox/Macros_Lisa/ManipsTM/cells/cell0003.tif")
+	imp5 = ImagePlus("/Users/lisalamasse/Dropbox/Macros_Lisa/ManipsTM/cells/cell0004.tif")
+	imp6 = ImagePlus("/Users/lisalamasse/Dropbox/Macros_Lisa/ManipsTM/cells/cell0005.tif")
+	imp7 = ImagePlus("/Users/lisalamasse/Dropbox/Macros_Lisa/ManipsTM/cells/cell0006.tif")
+	imp8 = ImagePlus("/Users/lisalamasse/Dropbox/Macros_Lisa/ManipsTM/cells/cell0007.tif")
+	imp9 = ImagePlus("/Users/lisalamasse/Dropbox/Macros_Lisa/ManipsTM/cells/cell0008.tif")
+	imp10 = ImagePlus("/Users/lisalamasse/Dropbox/Macros_Lisa/ManipsTM/cells/cell0009.tif")
+	imp11 = ImagePlus("/Users/lisalamasse/Dropbox/Macros_Lisa/ManipsTM/cells/cell0010.tif")
+	imp12 = ImagePlus("/Users/lisalamasse/Dropbox/Macros_Lisa/ManipsTM/cells/cell0011.tif")
+	
+
+	listimp=[imp1,imp2,imp3, imp4, imp5, imp6, imp7, imp8, imp9, imp10, imp11, imp12 ]
+
+	toto = ListeImage(listimp)
+	toto.show()
+	
+	
+	for imp in listimp :
+		print imp.getOriginalFileInfo().directory , imp.getOriginalFileInfo().fileName
